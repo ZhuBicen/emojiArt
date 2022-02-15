89e47efe38a24492626c3fa3059428bd23280bd1
Zhu Bicen <zhubicen@gmail.com>
2022-02-15 21:56:13 +0800

    Initial Commit

---
+++ emojiArt/emojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiArtApp.swift
+//  emojiArt
+//
+//  Created by Bicen Zhu on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct emojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
